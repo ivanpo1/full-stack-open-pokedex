6573dfe27a8a5442113b44fef85144e084667584
ivanpo1 <ivan.poppino@gmail.com>
2025-12-02 15:51:58 -0300

    test: add test

--- e2e-tests/test.spec.js
+++ e2e-tests/test.spec.js
@@ -6,4 +6,11 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+  test(' can navigate from the main page to the page of a particular Pokemon', async ({ page }) => {
+    await page.goto('http://localhost:8080/pokemon/ivysaur')
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+    await page.goto('http://localhost:8080/pokemon/charmander')
+    await expect(page.getByText('solar power')).toBeVisible()
+  })
 })
+
