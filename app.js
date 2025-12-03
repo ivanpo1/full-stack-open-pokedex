fa0d111da87e4b77882a39f513d6940d4cf930f9
ivanpo1 <ivan.poppino@gmail.com>
2025-12-03 10:02:06 -0300

    ci: breaking change

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('health check: v1')
+  res.send('breaking health check: v2')
 })
 
 app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
